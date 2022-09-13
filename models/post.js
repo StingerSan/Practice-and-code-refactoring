dcd625e3bbe8325225d83b2eaf1a15e19f9ddbef
StingerSan <stinger@int.pl>
2022-09-13 09:07:22 +0200

    Implementing  update and delete to the model

--- models/post.js
+++ models/post.js
@@ -1,18 +1,47 @@
+// const { ObjectID } = require("bson");
+const mongodb = require("mongodb");
 const db = require("../data/database");
+
+const ObjectID = mongodb.ObjectId;
 
 class Post {
   constructor(tittle, content, id) {
     this.tittle = tittle;
     this.content = content;
-    this.id = id;
+
+    if (id) {
+      this.id = new ObjectID(id);
+    }
   }
 
   async save() {
-    const result = await db.getDb().collection("posts").insertOne({
-      title: this.tittle,
-      content: this.content,
-    });
+    let result;
+    if (this.id) {
+      result = await db
+        .getDb()
+        .collection("posts")
+        .updateOne(
+          { _id: this.id },
+          { $set: { title: this.title, content: this.content } }
+        );
+    } else {
+      result = await db.getDb().collection("posts").insertOne({
+        title: this.tittle,
+        content: this.content,
+      });
+    }
 
+    return result;
+  }
+
+  async delete() {
+    if (!this.id) {
+      return;
+    }
+    const result = await db
+      .getDb()
+      .collection("posts")
+      .deleteOne({ _id: this.id });
     return result;
   }
 }
