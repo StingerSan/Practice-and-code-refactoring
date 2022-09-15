1a1918545b501618f729280a0024e054fcf3deee
StingerSan <stinger@int.pl>
2022-09-15 09:19:55 +0200

    Refactoring sessions and input validation errors

--- utility/validation-session.js
+++ utility/validation-session.js
@@ -1,11 +1,10 @@
-function getSessionErrorData(req) {
+function getSessionErrorData(req, defaultValues) {
   let sessionInputData = req.session.inputData;
 
   if (!sessionInputData) {
     sessionInputData = {
       hasError: false,
-      title: "",
-      content: "",
+      ...defaultValues,
     };
   }
 
@@ -14,6 +13,16 @@ function getSessionErrorData(req) {
   return sessionInputData;
 }
 
+function flashErrorsToSession(req, data, action) {
+  req.session.inputData = {
+    hasError: true,
+    ...data,
+  };
+
+  req.session.save(action);
+}
+
 module.exports = {
   getSessionErrorData: getSessionErrorData,
+  flashErrorsToSession: flashErrorsToSession,
 };
